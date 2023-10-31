3940273a3acdcb4aad5871e556109080b62dee36
Shanda Shaw <srh0029@hotmail.com>
2023-10-30 22:37:25 -0500

    updated Home page message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-    return 
+    return (
+        <div className="home">
+            <h1>Explore the Star Wars Universe</h1>
+            
+        </div>
+    )
 }
 
 export default Home
